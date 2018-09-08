0d92dda1b75f38033ca77ee40e0b78417115e94e
Dmitry Vysotsky <bigbenperl@gmail.com>
2018-09-08 14:14:41 +0300

    Increase elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GriffinBot.MixProject do
     [
       app: :griffin_bot,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
