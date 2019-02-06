a1c10cf36a041059173a4bbc4628de5e6c3931cb
Dmitry Vysotsky <bigbenperl@gmail.com>
2019-02-06 16:48:10 +0300

    Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GriffinBot.MixProject do
   def project do
     [
       app: :griffin_bot,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
